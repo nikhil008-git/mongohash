d6e51108127519fe99db216cf9213d169241f631
nikhil <gittthub008@gmail.com>
2025-09-01 19:02:16 +0000

    chnages

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+import express from 'express'
+import mongoose from 'mongoose'
 
+const app = express()
+
+app.use(express.json())
+
+app.post('/signup',(req,res)=>{
+
+}) 
+
+app.post('/signup',(req,res)=>{
+
+})
+
+app.post('/todo',function(req,res){
+
+})
+app.get('/todos',(req,res)=>{
+
+})

54cf71b7b88c9079a42ffa21022a6f70b8cd31d0
nikhil <gittthub008@gmail.com>
2025-09-02 16:38:15 +0000

    changes

--- index.js
+++ index.js
@@ -1,60 +1,28 @@
-import express from 'express'
-import mongoose from 'mongoose'
-const { UserModel, TodoModel } = require("./db");
+olution
+const express = require("express");
 
-const app = express()
+const app = express();
+app.use(express.json());
 
-app.use(express.json())
-
-const JWT_SECRET = "s3cret";
-
-app.post("/signup", async function(req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
-    const name = req.body.name;
-
-    await UserModel.create({
-        email: email,
-        password: password,
-        name: name
-    });
+app.post("/signup", function(req, res) {
     
-    res.json({
-        message: "You are signed up"
-    })
 });
 
 
-app.post("/signin", async function(req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
+app.post("/signin", function(req, res) {
 
-    const response = await UserModel.findOne({
-        email: email,
-        password: password,
-    });
-
-    if (response) {
-        const token = jwt.sign({
-            id: response._id.toString()
-        })
-
-        res.json({
-            token
-        })
-    } else {
-        res.status(403).json({
-            message: "Incorrect creds"
-        })
-    }
 });
-app.post('/todo',function(req,res){
 
-})
-app.get('/todos',(req,res)=>{
 
-})
+app.post("/todo", function(req, res) {
+
+});
+
+
+app.get("/todos", function(req, res) {
+
+});
 
 app.listen(3000,()=>{
-    console.log('Server is running on port 3000')
-})
+    console.log("Server is running on port 3000");
+});

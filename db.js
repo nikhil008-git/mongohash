2bc2f9347b14ea554163338abbbbccac704596b1
nikhil <gittthub008@gmail.com>
2025-09-03 05:57:05 +0000

    db change

---
+++ db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const user = new Schema({
+    name : String,
+    email : { type : String, required : true },
+    password : String
+})
+
+const Todo = new Schema ({
+    userId : ObjectId,
+    title : String,
+    done : Boolean
+})
+
+const UserModel = mongoose.model('users', user)
+const TodoModel = mongoose.model('todos',todo)
+
+module.exports = {
+    UserModel,
+    TodoModel
+}
